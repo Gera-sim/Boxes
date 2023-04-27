91b476baef6e20299755b0dd57d6b8c1487ca381
Gera-sim <g-926@ya.ru>
2023-04-27 13:58:00 +0300

    code with depricated

---
+++ app/src/main/java/com/example/boxes/BoxActivity.kt
@@ -0,0 +1,23 @@
+package com.example.boxes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.boxes.databinding.ActivityBoxBinding
+
+class BoxActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityBoxBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBoxBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.toMainMenuButton.setOnClickListener {  onToMainMenuPressed() }
+    }
+
+    private fun onToMainMenuPressed() {
+        val intent = Intent(this, MenuActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
+    }}
